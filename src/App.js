02e10e0083084fa9f7dcfbc9c02f8b6cb3ee98b2
Anniina K <89989566+sprungle@users.noreply.github.com>
2022-09-09 16:15:37 +0300

    first commit, all works

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [age, setAge] = useState(0);
+  const [upperLimit, setUpperLimit] = useState(0);    
+  const [lowerLimit, setLowerLimit] = useState(0);    
+
+  function calculate(){
+    const resultUpperLimit = (220 - age) * 0.85;
+    const resultLowerLimit = (220 - age) * 0.65;
+    setLowerLimit (resultLowerLimit);
+    setUpperLimit (resultUpperLimit);
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <h3> Heart rate limits calulcator</h3>
+    <p> Type in your age to know your heart rate limits</p>
+      <form>
+        <div>
+          <label>Age </label>
+          <br></br>
+          <input type="number" value={age} onChange={e => setAge(e.target.value)} />
+        </div>
+        <br></br>
+        <div>
+          <label>Heart rate limits are: </label>
+            <div>
+              <output>{lowerLimit.toFixed(0)} - </output> 
+              <output>{upperLimit.toFixed(0)}</output> 
+            </div>
+        </div>
+        <button type="button" onClick={calculate}>Calculate</button>
+      </form>
+    
+  </div>
+
+    
   );
 }
 
